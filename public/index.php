729784809e0d09bed70b83b49cd85d2818ba3dd4
Ahliman <ahliman.huseynov@laposte.net>
2019-11-04 11:51:06 +0100

    assets moved to public and url detect with php

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once(__DIR__ . "/../vendor/autoload.php");
+define('APP_DIR', __DIR__ . '/..');
+
+$uri = $_SERVER['REQUEST_URI'];
+
+$uri = explode("/", trim($uri, "/"));
+
+if (!$uri[0]) { $uri[0] = "index"; }
+
+try {
+    require_once "../templates/".$uri[0].".phtml";
+}
+catch (Exception $exception){
+    echo $exception;
+}
